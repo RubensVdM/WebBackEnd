f7d092fcd82970b546d63e3c367d8e1557f1f5b7
RubensVdM <rubenvdm@telenet.be>
2017-10-06 11:02:11 +0200

    String-Extra-Functions

---
+++ oplossingen/Oplossingen_oefeningen/String-Extra_Functions.php
@@ -0,0 +1,24 @@
+<?php
+$fruit = "kokosnoot";
+$aantalKaraktersFruit = strlen($kokonoot);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>String-extra-functions-1</title>
+</head>
+<body>
+<p>
+<?=
+$aantalKaraktersFruit;
+?>
+</p>
+    
+</body>
+</html>
