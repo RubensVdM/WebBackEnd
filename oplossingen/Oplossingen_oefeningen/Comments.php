ef9102d0be97bf8da98f3128e4e9ac7f14a341db
RubensVdM <rubenvdm@telenet.be>
2017-10-06 09:35:55 +0200

    Comments

---
+++ oplossingen/Oplossingen_oefeningen/Comments.php
@@ -0,0 +1,26 @@
+<?php
+$naam = "Ruben";
+$achternaam = "Van der Meirsch";
+# Ruben
+# rubenvdm@telenet.be
+
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comments</title>
+</head>
+<body>
+
+<p>
+<?=
+$naam ." ". $achternaam
+?>
+</p>
+    
+</body>
+</html>
