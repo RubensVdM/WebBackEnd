fac2fd7dda44ded8d19b1fbc2a85d97b39fb2d42
RubensVdM <rubenvdm@telenet.be>
2017-10-10 13:11:59 +0200

    Arrays-Basis

--- oplossingen/Oplossingen_oefeningen/Arrays-Basis.php
+++ oplossingen/Oplossingen_oefeningen/Arrays-Basis.php
@@ -15,6 +15,12 @@ $dieren2[] = "stier";
 $voertuigen['landvoertuigen'] = array ("vespa", "fiets");
 $voertuigen['watervoertuigen'] = array ("surfplank", "vlot", "schoener", "driemaster");
 $voertuigen['luchtvoertuigen'] = array ("luchtballon", "helicopter", "B52");
+
+$getallen = array(1, 2, 3, 4, 5);
+$getallen2 = array(5, 4, 3, 2, 1);
+$SumGetallen1En2 = array();
+$lengthOfGetallen = count($getallen);
+
 ?>
 
 
@@ -29,7 +35,29 @@ $voertuigen['luchtvoertuigen'] = array ("luchtballon", "helicopter", "B52");
 <body>
 <p>
 <?php
-var_dump($voertuigen);
+//var_dump($voertuigen);
+
+$productOfGetallen = array_product($getallen);
+echo "Dit is het product van alle getallen uit de array: " . $productOfGetallen;
+
+echo ". Dit zijn de oneven getallen uit de array: ";
+foreach($getallen as $number)
+{
+    if($number % 2 != 0)
+    {
+        echo $number;
+    } 
+}
+
+for($i = 0; $i < $lengthOfGetallen; $i++)
+{
+    $SumGetallen1En2[$i] = $getallen[$i] + $getallen2[$i];
+}
+echo ". Dit is de som van de 2 arrays: ";
+foreach($SumGetallen1En2 as $number)
+{
+    echo $number;
+}
 ?>
 </p>
 </body>
